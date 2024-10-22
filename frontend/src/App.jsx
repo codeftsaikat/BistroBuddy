e3ede884a55e30ced9fae0fa12ac4d468460e372
codeftsaikat <soikatdey18@gmail.com>
2024-10-21 19:52:51 -0700

    Setup core components of BistroBuddy web app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="text-center ">
+      <h1 className="text-4xl text-center">Vite + React</h1>
+      <div className="mt-20 flex gap-4 justify-center">
+      <button className="btn btn-outline">Default</button>
+      <button className="btn btn-outline btn-primary">Primary</button>
+      <button className="btn btn-outline btn-secondary">Secondary</button>
+      <button className="btn btn-outline btn-accent">Accent</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
