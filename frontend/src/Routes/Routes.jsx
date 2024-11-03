9f6598ecb899fc38bce2f00d73286da0aa181b8a
codeftsaikat <soikatdey18@gmail.com>
2024-11-02 21:16:23 -0700

    Implement login section

--- frontend/src/Routes/Routes.jsx
+++ frontend/src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Menu from "../Pages/Menu/Menu";
 import Order from "../Pages/Order/Order";
+import Login from "../Pages/Login/Login";
 
 
 export const router = createBrowserRouter([
@@ -21,7 +22,11 @@ export const router = createBrowserRouter([
       {
         path:"order/:category",
         element:<Order/>
-      }
+      },
+      {
+        path:"/login",
+        element:<Login/>
+      },
     ],
   },
 ]);
--- frontend/src/components/FoodCard/FoodCard.jsx
+++ frontend/src/components/FoodCard/FoodCard.jsx
@@ -11,8 +11,8 @@ const FoodCard = ({item}) => {
       </figure>
       <p className="absolute top-5 right-8 bg-black text-white text-xl font-medium py-1.5 px-4">${price}</p>
       <div className="card-body">
-        <h2 className="card-title">{name}</h2>
-        <p>{recipe}</p>
+        <h2 className="card-title self-center">{name}</h2>
+        <p className="text-base">{recipe}</p>
         <div className="card-actions justify-end">
           <button className="btn btn-outline border-0 border-b-4 mx-auto block text-yellow-400">Add to card</button>
         </div>
